9c69c75fff691b3be0dd8f5169ecfac3c968593c
davidcwh <dcwh12321@gmail.com>
2020-08-01 03:40:37 +0800

    Add Routes and Home Components

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Scrap</h1>
+        <p>A simple Note-taking App</p>
+      </div>
+    </div>
+  );
+}
