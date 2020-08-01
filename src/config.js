b3db60a3473f5c5be06ce532471c948636a9cdba
davidcwh <dcwh12321@gmail.com>
2020-08-01 14:20:43 +0800

    Add AWS Amplify Config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "scrap-uploads"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-1",
+      URL: "https://yimc9e8aj1.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-1",
+      USER_POOL_ID: "ap-southeast-1_oPOFmAyyP",
+      APP_CLIENT_ID: "3bccbe0ve79ub154so00k9g8fq",
+      IDENTITY_POOL_ID: "ap-southeast-1:2a02b271-b9ed-46cc-99c2-38afac645d15"
+    }
+  };
