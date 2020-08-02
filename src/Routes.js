c5c6ddbfadc94501e459275d84864d7c59013649
davidcwh <dcwh12321@gmail.com>
2020-08-03 00:15:59 +0800

    Add Signup front-end page

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 function Routes() {
@@ -14,6 +15,10 @@ function Routes() {
 
       <Route exact path="/login">
         <Login />
+      </Route>
+
+      <Route exact path="/signup">
+        <Signup />
       </Route>
 
       <Route>
